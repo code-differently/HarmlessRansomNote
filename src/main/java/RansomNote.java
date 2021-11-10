b2dade37f3504a8e27c47f86b5ff9c959156696c
Alvina Waseem <alvinawaseem@Alvinas-Air.fios-router.home>
2021-11-10 15:09:53 -0500

    done

--- src/main/java/RansomNote.java
+++ src/main/java/RansomNote.java
@@ -1,5 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class RansomNote {
     public Boolean harmlessRansomNote(String message , String magazineText){
-        return null;
+      String[] magazineArray = magazineText.split(" ");
+      String[] messageArray = message.split(" ");
+
+        Map<String, Integer> magazineParts = new HashMap<String, Integer>();
+        Map<String, Integer> messageParts = new HashMap<String, Integer>();
+
+        for(String word: magazineArray){
+            if (!magazineParts.containsKey(word)) magazineParts.put(word, 0);
+            magazineParts.put(word, magazineParts.get(word) + 1);
+        }
+
+        for(String word: messageArray){
+            if (!messageParts.containsKey(word)) messageParts.put(word, 0);
+            messageParts.put(word, messageParts.get(word) + 1);
+        }
+
+        for(String key: messageParts.keySet()){
+            if (!magazineParts.containsKey(key)) return false;
+            int needed = messageParts.get(key);
+            if (magazineParts.get(key) < needed) return false;
+        }
+
+        return true;
     }
 }
