a54302bdcda19ea354fa29319586adece7370cf3
Adonis-Cuello <adonisac2002@gmail.com>
2021-11-10 15:13:35 -0500

    could not find a solution

--- src/main/java/RansomNote.java
+++ src/main/java/RansomNote.java
@@ -1,5 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class RansomNote {
     public Boolean harmlessRansomNote(String message , String magazineText){
-        return null;
+        Boolean match = false;
+        Map<String, Integer> words = new HashMap<>();
+        String[] arr = magazineText.split(" ");
+        for (int i = 0; i < arr.length; i++){
+            String currentWord = arr[i];
+            if (words.containsKey(currentWord)){
+                Integer currentCount = words.get(currentWord);
+                currentCount++;
+                words.put(currentWord, currentCount);
+            } else
+                words.put(currentWord, 1);
+        }
+        String[] messageArray = message.split(" ");
+//        for (String wordsInMessage : messageArray){
+//            if (wordsInMessage.)
+//            match = true;
+//
+//        }
+        Map<String, Integer> resultMap = new HashMap<>();
+
+        for (String i : arr){
+            Integer j = words.get(i);
+            words.put(i, (j == null) ? 1 : j + 1);
+        }
+        for (Map.Entry<String, Integer> cor : words.entrySet()) {
+            System.out.println("Element" + cor.getKey() + " " + "occurs" + ": " + cor.getValue() + "times");
+            Boolean result = cor.getValue() > 1;
+        }
+
+        return match;
     }
 }
