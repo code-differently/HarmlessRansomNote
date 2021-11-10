1eb87f18e3c84e6d7cc6cb0778879a54b021769d
yborelandcdmac <yboreland19@archmerealumni.org>
2021-11-10 15:21:26 -0500

    stuck

--- src/main/java/RansomNote.java
+++ src/main/java/RansomNote.java
@@ -1,5 +1,44 @@
+import java.util.HashMap;
+
 public class RansomNote {
     public Boolean harmlessRansomNote(String message , String magazineText){
-        return null;
+        String[] text = magazineText.split(" ");
+        String[] ransom = message.split(" ");
+        boolean valid = false;
+        HashMap<String,Integer> words = new HashMap<>();
+       for(int i = 0; i < magazineText.length(); i ++){
+           //words.merge(text[i], 1, Integer::sum);
+           if(!words.containsKey(text[i])){
+               words.put(text[i],1);
+           }else{
+               Integer wrdAmt = words.get(text[i]);
+               words.put(text[i], wrdAmt + 1);
+           }
+       }
+        HashMap<String,Integer> Ranwords = new HashMap<>();
+       for(int i = 0; i < message.length(); i ++){
+
+           if(!Ranwords.containsKey(ransom[i])){
+               Ranwords.put(ransom[i],1);
+              // valid = false;
+           }else {
+               Integer wrdAmt = Ranwords.get(ransom[i]);
+               Ranwords.put(ransom[i], wrdAmt +1);
+              // valid = true;
+           }
+       }
+
+       for(int i = 0; i < message.length(); i ++ ){
+           if(!words.containsKey(ransom[i])){
+               valid = false;
+           }else {
+               valid = true;
+           }
+       }
+
+
+
+
+        return valid;
     }
 }
