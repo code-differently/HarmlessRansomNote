ca39baa7af0bc967f57200e14dc358fd0a2ed9e2
JonKappa <cappytwoj@gmail.com>
2021-11-10 19:03:38 -0500

    2

--- src/main/java/RansomNote.java
+++ src/main/java/RansomNote.java
@@ -3,12 +3,10 @@ import java.util.Map;
 
 public class RansomNote {
     public Boolean harmlessRansomNote(String message , String magazineText){
-        int messCount = 0;
-        int magCount = 0;
         String[] magazineWords = magazineText.split(" ");
         String[] messageWords = message.split(" ");
         Map<String, Integer> magazineMap = new HashMap<>();
-        for (int i = 0; i <= magazineText.length(); i++) {
+        for (int i = 0; i < magazineWords.length; i++) {
             if(magazineMap.containsKey(magazineWords[i])){
                 magazineMap.put(magazineWords[i], magazineMap.get(magazineWords[i])+1);
             }else{
@@ -16,21 +14,24 @@ public class RansomNote {
             }
         }
 
-        for (int i = 0; i <=messageWords.length; i++) {
+        for (int i = 0; i < messageWords.length; i++) {
             if (magazineMap.containsKey(messageWords[i])) {
                 magazineMap.put(messageWords[i], magazineMap.get(messageWords[i])-1);
                 if (magazineMap.get(messageWords[i]) < 0) {
-
+                    return false;
                 }
             }
+            else {
+                return false;
+            }
         }
+        return true;
 //        System.out.println(Arrays.toString(messageWords));
 //        System.out.println(Arrays.toString(magazineWords));
 //        for (int i = 0; i < messageWords.length; i++) {
 //            message.c
 //        }
 
-        return null;
 
 
     }
